711c759df50e3dfc6d8b60ef59e834474c0ea40c
RenzoSuganuma <suganumarenzo2004@gmail.com>
2023-06-19 14:14:34 +0900

    びっくり要素追加

---
+++ Assets/BossWarpToPlayerTrap.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SearchService;
+
+public class BossWarpToPlayerTrap : MonoBehaviour
+{
+    //ワープ先のトランスフォーム
+    [SerializeField] private Transform _targetTransform;
+    //ワープするオブジェクト
+    [SerializeField] GameObject _bossObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //プレイヤーがトラップのコライダー（トリガー）と接触したとき
+        if (other.gameObject.CompareTag("Player") && other.gameObject != null)
+        {
+            this._bossObject.SetActive(false);
+            this._bossObject.transform.position = this._targetTransform.position;
+            this._bossObject.SetActive(true);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(this.transform.position, 5);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(this._targetTransform.position, Vector3.one);
+    }
+}
